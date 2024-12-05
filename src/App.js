8e6456898f09b2f379f8d33e3b4e792a9e1788db
Brandon Benoit <brandon@digitalkatana.dev>
2024-12-04 18:41:23 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { randomPassword } from './util/helpers';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [show, setShow] = useState(false);
+	const [length, setLength] = useState('');
+	const [password, setPassword] = useState('');
+	const current = localStorage.getItem('current');
+
+	const handleReveal = () => {
+		setShow(!show);
+	};
+
+	const handleClick = () => {
+		localStorage.setItem('current', password);
+		setShow(true);
+		setLength('');
+		setPassword('');
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		// setPassword(randomPassword(length));
+		setPassword('haYLE3LiNdaB76');
+	};
+
+	return (
+		<div className='App'>
+			<button className='reveal-btn' onClick={handleReveal} />
+			{show && (
+				<div>
+					<h3>Windows:</h3>
+					<h4>{current}</h4>
+				</div>
+			)}
+			<form onSubmit={handleSubmit}>
+				<input
+					type='number'
+					placeholder='Enter length'
+					value={length}
+					onChange={(e) => setLength(e.target.value)}
+				/>
+				<button type='submit'>Generate</button>
+			</form>
+			{password && <h3 onClick={handleClick}>{password}</h3>}
+		</div>
+	);
 }
 
 export default App;
