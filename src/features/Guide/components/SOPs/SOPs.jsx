c2f67645687cfde83d220991231cc22a520b8e59
Brandon Benoit <brandon@digitalkatana.dev>
2025-01-18 13:03:20 -0800

    added Circuit Order SOP, refactoring

--- src/features/Guide/components/SOPs/SOPs.jsx
+++ src/features/Guide/components/SOPs/SOPs.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './sop.scss';
 import VoiceOrder from './components/VoiceOrder';
+import CircuitOrder from './components/CircuitOrder';
 
 const SOPs = () => {
 	const [view, setView] = useState('index');
@@ -13,11 +14,16 @@ const SOPs = () => {
 		<div id='sop'>
 			{view === 'vo' ? (
 				<VoiceOrder goBack={() => setView('index')} />
+			) : view === 'co' ? (
+				<CircuitOrder goBack={() => setView('index')} />
 			) : (
 				view === 'index' && (
 					<div className='sop-list'>
 						<h3 onClick={() => handleClick('vo')}>
 							How to Process a Voice Order
+						</h3>
+						<h3 onClick={() => handleClick('co')}>
+							How to Process a Circuit Order
 						</h3>
 					</div>
 				)
