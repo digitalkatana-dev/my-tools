b736043a817a62dde92a1eea8b3a42155229aefc
Brandon Benoit <brandon@digitalkatana.dev>
2025-02-28 08:39:22 -0800

    Update Auth.jsx

--- src/features/Auth/Auth.jsx
+++ src/features/Auth/Auth.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FormControl } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
-import { setEmail, setPassword, login, clearErrors } from '../../redux/slices/userSlice';
+import { setEmail, setPassword, login, clearErrors_User } from '../../redux/slices/userSlice';
 import TextInput from '../../components/TextInput';
 import Button from '../../components/Button';
 import './auth.scss';
@@ -13,7 +13,7 @@ const Auth = () => {
 	const dispatch = useDispatch();
 
 	const handleFocus = ()=> {
-		dispatch(clearErrors());
+		dispatch(clearErrors_User());
 	}
 
 	const handleChange = (input, value) => {
