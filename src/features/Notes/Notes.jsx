53872b3323837f0d80554e7f990b70c6eddc2b9e
Brandon Benoit <brandon@digitalkatana.dev>
2025-05-09 22:34:54 -0700

    added notes page

--- src/features/Notes/Notes.jsx
+++ src/features/Notes/Notes.jsx
@@ -1,39 +1,47 @@
-import React from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { clearSuccess_User } from '../../redux/slices/userSlice';
 import './notes.scss';
 import Button from '../../components/Button';
 import NoteItem from './components/NoteItem';
+import CreateNoteDialog from './components/CreateNoteDialog';
 
 const Notes = () => {
-	const { activeUser } = useSelector((state) => state.user);
+	const { activeUser, success } = useSelector((state) => state.user);
+	const [showDialog, setShowDialog] = useState(false);
 	const dispatch = useDispatch();
+	const userNotes = activeUser?.notes;
+
+	const handleOpenClick = () => {
+		setShowDialog(true);
+	};
+
+	const handleClose = () => {
+		setShowDialog(false);
+	};
+
+	const handleSuccess = useCallback(() => {
+		if (success) {
+			setShowDialog(false);
+			setTimeout(() => {
+				dispatch(clearSuccess_User());
+			}, 5000);
+		}
+	}, [success, dispatch]);
+
+	useEffect(() => {
+		handleSuccess();
+	}, [handleSuccess]);
 
 	return (
 		<div id='notes'>
-			<Button btnClass='new-note-trigger'>New Note</Button>
-			<NoteItem />
-			<NoteItem />
-			<NoteItem />
-			<NoteItem />
-			<NoteItem />
-			<NoteItem />
-			<NoteItem />
-			<NoteItem />
-			<NoteItem />
-			<NoteItem />
-			<NoteItem />
-			<NoteItem />
-			<NoteItem />
-			<NoteItem />
-			<NoteItem />
-			<NoteItem />
-			<NoteItem />
-			<NoteItem />
-			<NoteItem />
-			<NoteItem />
-			<NoteItem />
-			<NoteItem />
-			<NoteItem />
+			<Button btnClass='new-note-trigger' onClick={handleOpenClick}>
+				New Note
+			</Button>
+			{userNotes?.map((note) => (
+				<NoteItem key={note._id} data={note} />
+			))}
+			<CreateNoteDialog showDialog={showDialog} onClose={handleClose} />
 		</div>
 	);
 };
