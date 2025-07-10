9b696125a5dd1fa6ed931c155b0bc46cee299ed7
Brandon Benoit <brandon@digitalkatana.dev>
2025-07-09 19:40:23 -0700

    handeled expired auth token

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -1,3 +1,14 @@
+import { jwtDecode } from 'jwt-decode';
+
+export const isTokenExpired = (token) => {
+	if (!token) return true;
+
+	try {
+		const { exp } = jwtDecode(token);
+		return Date.now() >= exp * 1000;
+	} catch (e) {}
+};
+
 export const randomPassword = (length) => {
 	const characters =
 		'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
